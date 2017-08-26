2c7026a09bf0ef56053ecb275cdc478dc6df93fa
res0nance <res0nance@users.noreply.github.com>
2017-08-27 01:10:59 +0800

    Initial bot commit

---
+++ app.py
@@ -0,0 +1,26 @@
+import telepot
+import pprint
+import telepot.loop
+import time
+from googletrans import Translator
+import os
+
+bot = telepot.Bot(os.environ['telegram_apikey'])
+translator = Translator()
+target_language = 'en'
+
+def handle(msg):
+    pprint.pprint(msg)
+    if 'text' in msg:
+        message = msg['text']
+        r = translator.detect(message)
+        pprint.pprint(r.lang)
+        if r.lang != target_language:
+            translated = translator.translate(message,target_language)
+            bot.sendMessage(msg['chat']['id'], translated.text)
+
+pprint.pprint(bot.getMe())
+telepot.loop.MessageLoop(bot,handle).run_as_thread()
+
+while 1:
+    time.sleep(10)
