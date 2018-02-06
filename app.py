45b3274a909850f23eafd49095926462110f1b47
res0nance <res0nance@users.noreply.github.com>
2018-02-06 22:47:24 +0800

    Add more debug code

--- app.py
+++ app.py
@@ -72,11 +72,13 @@ def translate_text(text):
     data = ''.join(c for c in unicodedata.normalize('NFC', text) if c <= '\uFFFF')
     r = translator.detect(data)
     conf = get_required_confidence(data)
+    pprint.pprint(data)
     pprint.pprint(r.lang)
     pprint.pprint(r.confidence)
     pprint.pprint(conf)
     if r.lang != target_language and r.confidence >= conf:
         transtext = translator.translate(data,target_language).text
+        pprint.pprint(transtext)
         if transtext.lower().strip() != data.lower().strip():
             conf_rating = str(round(r.confidence,2))
             trans_msg = transtext + ' (' + pycountry.languages.get(alpha_2 = r.lang[:2]).name + ' : ' + conf_rating + ')'
