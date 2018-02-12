42a893df93ad1310cf39bc18692df706f7fbc7bd
res0nance <res0nance@users.noreply.github.com>
2018-02-12 18:36:06 +0800

    Edit messages?

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def handle_command(text,id):
             buttons = []
             results = wikipedia.search(search_param)
             for result in results[1:]:
-                buttons.append([telepot.namedtuple.InlineKeyboardButton(text=result, callback_data=str(id) + " " + result)])
+                buttons.append([telepot.namedtuple.InlineKeyboardButton(text=result, callback_data=result)])
             bot.sendMessage(id, "Results for "+ search_param, reply_markup=telepot.namedtuple.InlineKeyboardMarkup(inline_keyboard=buttons))
         except wikipedia.exceptions.PageError:
             bot.sendMessage(id, "No results found for " + search_param)
@@ -119,8 +119,8 @@ def on_callback_query(msg):
     query_id, from_id, data = telepot.glance(msg, flavor='callback_query')
     print('Callback query:', query_id, from_id, data)
     pprint.pprint(msg)
-    split_data = data.split()
-    bot.sendMessage(int(split_data[0]), wikipedia.summary(' '.join(split_data[1:])))
+    msg_idf = telepot.message_identifier(msg['message'])
+    bot.editMessage(msg_idf, wikipedia.summary(data))
 
 def handle(msg):
     pprint.pprint(msg)
