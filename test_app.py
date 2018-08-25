66acf0116e75a2cf54a7b2c7698d9ef24528c00e
Raihaan <31362124+res0nance@users.noreply.github.com>
2018-08-25 17:24:29 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -67,3 +67,7 @@ def test_lyrics1():
 def test_lyrics2():
     app.test_mode()
     app.handle_command('/lyrics Rapid Elemental Dissolve', 0)
+
+def test_no_command():
+    app.test_mode()
+    assert app.handle_command('peanut', 0) == False
