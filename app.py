1de125b3f87ed690620b1fffe548b8d089020352
Raihaan Shouhell <raihaanhimself@gmail.com>
2020-04-08 18:04:50 +0800

    Allow russian

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import animelyrics
 
 target_language = 'en'
 isascii = lambda s: len(s) == len(s.encode())
-allowed_languages = ['zh', 'ja', 'ms', 'id', 'tl', 'fr']
+allowed_languages = ['zh', 'ja', 'ms', 'id', 'tl', 'fr', 'ru']
 
 
 bot        = None
